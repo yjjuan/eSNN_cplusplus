aacc7a4278fb5f3934dfd1bf85c66a79af6f8620
kevin.huang <kevin.huang>
2020-02-03 07:56:20 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,11 +29,9 @@ int main() {
 
 
     //comment or uncoment lines 31-63 base on benchmark you want to test
-/*
-    string path = "F:\\artykuly\\Anomaly detection eSNN\\Software\\eSNN-RTAD"
-                  "\\Datasets\\numenta\\";
-    string resultsPath = "F:\\artykuly\\Anomaly detection eSNN\\Software\\eSNN-RTAD"
-                         "\\Results\\numenta_fMeasure_optimization\\";
+
+    string path = "./datasets/NAB";
+    string resultsPath = "./models_zoo";
     vector<string> folders = {
             "artificialNoAnomaly",
            "artificialWithAnomaly",
@@ -43,7 +41,7 @@ int main() {
             "realTraffic",
             "realTweets"
     };
-*/
+
 
 /*    string path = "F:\\artykuly\\Anomaly detection eSNN\\Software\\eSNN-RTAD"
                   "\\Datasets\\Yahoo\\";
@@ -67,7 +65,7 @@ int main() {
         
 
         //below set parameters for the selected benchmark
-/*  //Numenta benchmark parameters set
+  //Numenta benchmark parameters set
         double NOsize_b = 50, NOsize_e = 50, NOsize_s = 100; //parameters for grid search (xxx_b - intial, xxx_e - ending, xxx_s - step)
         double Wsize_b = 100, Wsize_e = 600, Wsize_s = 100;
         double NIsize_b = 10, NIsize_e = 10, NIsize_s = 20;
@@ -78,7 +76,7 @@ int main() {
         double C_b = 0.6, C_e = 0.6, C_s = 0.1;
         double ErrorFactor_b = 0.9, ErrorFactor_e = 0.9, ErrorFactor_s = 0.1;
         double AnomalyFactor_b = 2, AnomalyFactor_e = 7, AnomalyFactor_s = 1;
-*/
+
 
 /*    //Yahoo benchmark parameters set
        double NOsize_b = 50, NOsize_e = 50, NOsize_s = 100; //parameters for grid search (xxx_b - intial, xxx_e - ending, xxx_s - step)
